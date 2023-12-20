f2c2a89fbb8b3b83ddb76c89140a0cfdf0f8b9a1
Ali Omar <ali.noor.student.moringaschool.com git config --gobal Ali abdullahigit config --global user.email ali.noor.student.moringaschool.com>
2023-12-20 18:46:34 +0300

    done

---
+++ lib/migrations/versions/e7fc8d3faa7b_renaming_students_to_scholars.py
@@ -0,0 +1,24 @@
+"""Renaming students to scholars
+
+Revision ID: e7fc8d3faa7b
+Revises: dcf175d68a1f
+Create Date: 2023-12-20 01:36:51.048123
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e7fc8d3faa7b'
+down_revision = 'dcf175d68a1f'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
